fa96c32415e769509664b5c303b39205bf0f80a2
brians dream <destarock1@gmail.com>
2022-01-11 00:22:33 +0700

    This is my firstime code with PHP

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>first time learning php 11 januari 2022</title>
+</head>
+<body>
+   
+
+
+<?php
+
+    echo  " <h1> This is my firstime learning php  </h1>";
+?> 
+
+</body>
+</html>
